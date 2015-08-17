a06f6c82f96142145c39b7f4f3df0429f57e90c6
kaosad <frasker@hotmail.com>
2015-08-17 17:49:27 +0800

    Modified rakefile to retrieve info from the file arguments

--- FlashProgrammer.rb
+++ FlashProgrammer.rb
@@ -2,12 +2,14 @@
 desc 'Make main.exe'
 task :main => [ :killer, :killer2, "Main.exe"] do
   puts('dummy')
+#  s = ['name', 'quest', 'favorite_color', '', 'speed', 'err/vello.o'].reject{|s| s =~ /(.*)\.o/}.join(' ')
+#  puts s
 end
 
 desc 'Clean main'
 task :clean_main do
-  sh "rm Main.exe"
-  sh "rm src/*.o"
+  sh "rm -f Main.exe"
+  sh "rm -f src/*.o"
 end
 
 task :killer2 do
@@ -18,20 +20,23 @@ task :killer do
   puts('killer')
 end
 
-file "src/Main.o" => ["src/Main.c", "src/Silly.h"]  do
-  compile_string = "gcc -c src/Main.c -o src/Main.o"
+file "src/Main.o" => ["src/Main.c", "src/Silly.h"]  do  |t|
+  # Reject any prerequisites ending with .h
+  compile_string = "gcc -c " + t.prerequisites.reject{|s| s =~ /(.*)\.h/}.join(' ') + " -o " + t.name
   puts(compile_string)
   sh compile_string
 end
 
-file "src/Silly.o" => ["src/Silly.c", "src/Silly.h"]  do
-  compile_string = "gcc -c src/Silly.c -o src/Silly.o"
+file "src/Silly.o" => ["src/Silly.c", "src/Silly.h"]  do |t|
+  # Reject any prerequisites ending with .h
+  compile_string = "gcc -c " + t.prerequisites.reject{|s| s =~ /(.*)\.h/}.join(' ') + " -o " + t.name
   puts(compile_string)
   sh compile_string
 end
 
-file "Main.exe" => ["src/Main.o", "src/Silly.o"]  do
-  compile_string = "gcc src/Silly.o src/Main.o -o Main.exe"
+file "Main.exe" => ["src/Main.o", "src/Silly.o"]  do |t|
+#  compile_string = "gcc src/Silly.o src/Main.o -o " + t.name
+  compile_string = "gcc " + t.prerequisites.join(' ') + " -o " + t.name
   puts(compile_string)
-  sh compile_string  
+  sh compile_string
 end
